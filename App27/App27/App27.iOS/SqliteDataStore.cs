47ae787d8728117a598362bc447ae3fbfb3112f4
XfHua <jasonHuaxf@163.com>
2019-10-30 15:30:13 +0800

    Add files via upload

---
+++ App27/App27/App27.iOS/SqliteDataStore.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App27.iOS;
+using Foundation;
+using SQLite;
+using UIKit;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SqliteDataStore))]
+
+namespace App27.iOS
+{
+    public class SqliteDataStore : IDataStore<Item>
+    {
+        private SQLiteConnection _db;
+
+        public SqliteDataStore()
+        {
+            _db = new SQLiteConnection(Path.Combine(FileSystem.AppDataDirectory, "items.sqlite"));
+            _db.CreateTable<Item>();
+            if (_db.Table<Item>().Count().Equals(0))
+            {
+                _db.InsertAll(new List<Item>
+            {
+                new Item { Id = 1, Text = "First item", Description = "This is the first item description." },
+                new Item { Id = 2, Text = "Second item", Description = "This is the second item description." },
+                new Item { Id = 3, Text = "Third item", Description = "This is the third item description." }
+            }
+                );
+            }
+        }
+
+        public async Task<bool> AddItemAsync(Item item)
+        {
+            _db.Insert(item);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItem(string id)
+        {
+            _db.Delete<Item>(id);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<Item> GetItemAsync(int id)
+        {
+            return await Task.FromResult(_db.Get<Item>(id));
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(_db.Table<Item>().ToList());
+        }
+
+        public async Task<bool> UpdateItemAsync(Item item)
+        {
+            _db.Update(item);
+            return await Task.FromResult(true);
+        }
+
+    }
+}
---
+++ App27/App27/App27/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App27
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DependencyService.Get<IDataStore<Item>>().AddItemAsync(new Item() {Id = 4, Text = "test", Description = "four-test" });
+
+            Task<Item> i = DependencyService.Get<IDataStore<Item>>().GetItemAsync(1);
+            Item myItem = i.Result;
+            Console.WriteLine("-----"+myItem.Text +myItem.Description);
+
+            Task<Item> fourthItem = DependencyService.Get<IDataStore<Item>>().GetItemAsync(4);
+            Item myItem4 = fourthItem.Result;
+            Console.WriteLine("-----"+myItem4.Text + myItem4.Description);
+        }
+
+    }
+
+    public interface IDataStore<Item>
+    {
+        Task<bool> AddItemAsync(Item item);
+        Task<bool> DeleteItem(string id);
+
+        Task<Item> GetItemAsync(int id);
+    }
+
+    public class Item
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
